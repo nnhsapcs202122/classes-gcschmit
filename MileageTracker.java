b3628315b77ee8169b40d3085150d7a7995b8697
Geoff Schmit <gcschmit@gmail.com>
2021-09-23 10:22:56 -0500

    define instance variables, constructors

--- MileageTracker.java
+++ MileageTracker.java
@@ -8,24 +8,62 @@ public class MileageTracker
 {
     /*
      * 2. Define the instance variables (store the object's attributes):
+     *      specify the visibility (e.g., private)
+     *          public: accessible by any code in any class
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double, String)
+     *      specify the name (e.g., milesDriven, vin)
+     *      
+     *  Instance variables differ from local variables in the following ways:
+     *      scoped to the class (accessible in all methods of the class;
+     *          lifetime is the same as the object)
+     *      automatically initialized to a default value (0, false, null)
+     *      best practice is not to immediately initialize instance variables
      */
-
-
-
-
-
-
+    private double milesDriven;     // in unit of miles
+    private double fuelConsumed;    // in units of gallons
+    private String vin;     // vehicle identification
     
     /*
      * 3. Define the constructor(s):
+     *      responsible for initializing newly created objects
+     *      invoked automatically via the new operator
+     *      name of the constructor must match the name of class
+     *      has no return type (not even void)
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another constructor (with restrictions)
      */
     
+    /**
+     * Default constructor for the MileageTracker class.
+     *      Initializes the object's miles driven and fuel consumed to 0.0
+     *          and the VIN to null.
+     */
+    public MileageTracker()
+    {
+        /*
+         * The "this" reserved word references the current object
+         *      (like "self" in Python)
+         *  Its usage is encouraged but not always required.
+         */
+        this.milesDriven = 0.0;
+        this.fuelConsumed = 0.0;
+        this.vin = null;
+    }
     
-    
-    
-    
-    
-    
+    /**
+     * Constructs a new MileageTracker object with the specified
+     *      miles driven and fuel consumed.
+     *      
+     *  @param initialMilesDriven the number of miles already driven
+     *  @param initialFuelConsumed the number of gallons of fuel already consumed
+     */
+    public MileageTracker(double initialMilesDriven, double initialFuelConsumed)
+    {
+        this.milesDriven = initialMilesDriven;
+        this.fuelConsumed = initialFuelConsumed;
+        this.vin = null;
+    }
     
     /*
      * 1. Define methods by specifying:
@@ -42,7 +80,8 @@ public class MileageTracker
      */
     public void incrementMilesDriven(double miles)
     {
-        
+        //this.milesDriven = this.milesDriven + miles;
+        this.milesDriven += miles;
     }
     
     /**
@@ -52,7 +91,7 @@ public class MileageTracker
      */
     public double getMilesDriven()
     {
-        return 0.0;
+        return this.milesDriven;
     }
     
     /**
@@ -62,7 +101,7 @@ public class MileageTracker
      */
     public void incrementFuelConsumed(double gallons)
     {
-        //this.fuelConsumed += gallons;
+        this.fuelConsumed += gallons;
     }
     
     /**
@@ -72,8 +111,7 @@ public class MileageTracker
      */
     public double getFuelConsumed()
     {
-        //return this.fuelConsumed;
-        return 0.0;
+        return this.fuelConsumed;
     }
     
     
@@ -84,9 +122,8 @@ public class MileageTracker
      */
     public double getMileage()
     {
-        //double mileage = this.fuelConsumed / this.milesDriven;
-        //return mileage;
-        return 0.0;
+        double mileage = this.milesDriven / this.fuelConsumed;
+        return mileage;
     }
     
     /**
