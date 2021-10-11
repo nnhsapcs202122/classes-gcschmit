5980f871096a61c51c3f94d1a6cc2cdcd0814bb5
Geoff Schmit <gcschmit@gmail.com>
2021-10-11 08:55:31 -0500

    Create DistanceConverter.java

---
+++ DistanceConverter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DistanceConverter
+{
+    public static void main(String[] args)
+    {
+        final int FEET_FOR_EVERY_YARD = 3;
+        final int INCHES_FOR_EVERY_FOOT = 12;
+        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of yards to be converted to feet and inches: ");
+
+        double yards = s.nextDouble();
+        double feet = yards * FEET_FOR_EVERY_YARD;
+        double inches = feet * INCHES_FOR_EVERY_FOOT;
+
+        System.out.println(yards + " yards are " + feet + " feet");
+        System.out.println(yards + " yards are " + inches + " inches");
+    }
+}
